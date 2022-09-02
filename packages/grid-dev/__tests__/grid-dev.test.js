e4862fc3c3a0931556d0db2f952cfaf5e9f25168
Luke Leber <lal65@psu.edu>
2022-09-02 13:36:38 -0400

    Remove dev packages

--- packages/grid-dev/__tests__/grid-dev.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const gridDev = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(gridDev(), 'Hello from gridDev');
-console.info("gridDev tests passed");
--- packages/grid-dev/lib/grid-dev.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = gridDev;
-
-function gridDev() {
-    return "Hello from gridDev";
-}
