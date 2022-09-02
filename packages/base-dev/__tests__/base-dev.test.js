7b93e681c7551103f95f4332659c978eeda5c52c
Luke Leber <lal65@psu.edu>
2022-09-02 12:32:39 -0400

    Make base component

--- packages/base-dev/__tests__/base-dev.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const baseDev = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(baseDev(), 'Hello from baseDev');
-console.info("baseDev tests passed");
--- packages/base-dev/lib/base-dev.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = baseDev;
-
-function baseDev() {
-    return "Hello from baseDev";
-}
