6200da5d7be608f140507ac952f82b5b315fc500
Luke Leber <lal65@psu.edu>
2022-09-12 09:47:17 -0400

    Add drop-button component

---
+++ packages/drop-button/src/js/scripts.js
@@ -0,0 +1,16 @@
+(cms => {
+  cms.attach('dropbutton', context => {
+    const drop_buttons = context.querySelectorAll('.drop-button');
+    drop_buttons.forEach(drop_button => {
+      const toggle = drop_button.querySelector('.drop-button__toggle');
+      toggle.addEventListener('click', () => {
+        if (toggle.getAttribute('aria-expanded') === 'true') {
+          toggle.setAttribute('aria-expanded', 'false');
+        }
+        else {
+          toggle.setAttribute('aria-expanded', 'true');
+        }
+      });
+    });
+  });
+})(cms);
