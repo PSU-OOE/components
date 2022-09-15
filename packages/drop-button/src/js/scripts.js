b1a02858e9bfa33ecc26003964d0eab28b3afea7
Luke Leber <lal65@psu.edu>
2022-09-15 07:36:28 -0400

    Oops.

--- packages/drop-button/src/js/scripts.js
+++ packages/drop-button/src/js/scripts.js
@@ -3,6 +3,8 @@
     const drop_buttons = context.querySelectorAll('.drop-button');
     drop_buttons.forEach(drop_button => {
       const toggle = drop_button.querySelector('.drop-button__toggle');
+
+      // Toggle the expanded state of drop-button menus on click.
       toggle.addEventListener('click', () => {
         if (toggle.getAttribute('aria-expanded') === 'true') {
           toggle.setAttribute('aria-expanded', 'false');
@@ -12,14 +14,16 @@
         }
       });
 
+      // Close an open drop-button menu when escape is pressed.
       drop_button.addEventListener('keydown', e => {
         if (e.key.toLowerCase() === 'escape' && toggle.getAttribute('aria-expanded') === 'true') {
           toggle.setAttribute('aria-expanded', 'false');
         }
       });
 
+      // Close an open drop-button menu when focus leaves the component.
       drop_button.addEventListener('focusout', (e) => {
-        if (!drop_button.contains(event.relatedTarget)) {
+        if (!drop_button.contains(e.relatedTarget)) {
           toggle.setAttribute('aria-expanded', 'false');
         }
       });
