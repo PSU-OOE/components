f5b29bbab9aaeb0341c2880ee2ddd6ad79add015
Luke Leber <lal65@psu.edu>
2022-09-23 11:03:53 -0400

    Polyfill test

--- packages/polyfill-focus-visible/src/js/scripts.js
+++ packages/polyfill-focus-visible/src/js/scripts.js
@@ -2,25 +2,21 @@
  * @file
  * Provides a performant focus-visible polyfill loader.
  */
-(Drupal => {
-  Drupal.behaviors.polyfillFocusVisible = {
-    attach: context => {
-      if (context === document) {
-        const style = document.createElement('style');
-        document.head.appendChild(style);
-        try {
-          style.sheet.insertRule(':focus-visible { color: initial }');
-          style.sheet.deleteRule(0);
-        }
-        catch (e) {
-          const script = document.createElement('script');
-          script.src = '/themes/custom/worldcampus/components/polyfill-focus-visible/vendor/focus-visible@5.2.0.min.js';
-          document.body.appendChild(script);
-        }
-        finally {
-          document.head.removeChild(style);
-        }
-      }
-    },
-  };
-})(Drupal);
+(cms => {
+  cms.attach('polyfillFocusVisible', context => {
+    const style = document.createElement('style');
+    document.head.appendChild(style);
+    try {
+      style.sheet.insertRule(':focus-visible { color: initial }');
+      style.sheet.deleteRule(0);
+    }
+    catch (e) {
+      const script = document.createElement('script');
+      script.src = cms.vendor_dir + '/focus-visible/dist/focus-visible.min.js';
+      document.body.appendChild(script);
+    }
+    finally {
+      document.head.removeChild(style);
+    }
+  });
+})(cms);
