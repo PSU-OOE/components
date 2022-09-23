c5842e1dfceb93d63a0c776e010aeb8c2f30c017
Zachary Ishler <zri5004@psu.edu>
2022-09-23 01:06:37 -0400

    Add expandable-section component (needs polyfill-focus-visible merged)

---
+++ packages/expandable-section/src/js/scripts.js
@@ -0,0 +1,41 @@
+((Drupal, cms) => {
+  Drupal.behaviors.expandableSection = {
+    attach: context => {
+      const elements = context.querySelectorAll('.expandable-section');
+      elements.forEach(element => {
+        const expand = element.querySelector('.expandable-section__expand');
+        const content = element.querySelector('.expandable-section__content');
+        const collapse = element.querySelector('.expandable-section__collapse');
+
+        expand.addEventListener('click', () => {
+          content.style['transition-duration'] = (content.scrollHeight / 2) + 'ms';
+          collapse.setAttribute('aria-expanded', 'true');
+          expand.setAttribute('aria-expanded', 'true');
+          cms.expand(content);
+          collapse.style.display = 'block';
+          expand.style.display = 'none';
+
+          // Say something for A/T users.
+          Drupal.announce('expanded', 'assertive');
+
+        });
+
+        collapse.addEventListener('click', () => {
+          content.style['transition-duration'] = (content.scrollHeight / 2) + 'ms';
+
+          collapse.setAttribute('aria-expanded', 'false');
+          expand.setAttribute('aria-expanded', 'false');
+          cms.collapse(content);
+
+          function afterCollapse() {
+            content.removeEventListener('transitionend', afterCollapse);
+            expand.style.display = 'block';
+            expand.focus();
+            collapse.style.display = 'none';
+          }
+          content.addEventListener('transitionend', afterCollapse);
+        });
+      });
+    }
+  };
+})(Drupal, cms);
