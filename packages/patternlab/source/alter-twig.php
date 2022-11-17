94646837eac970c3da1941ea99027c24762c2b4e
Luke Leber <lal65@psu.edu>
2022-11-17 17:03:53 -0500

    Be sure to read from manifest...not globbed fs

--- packages/patternlab/source/alter-twig.php
+++ packages/patternlab/source/alter-twig.php
@@ -6,20 +6,30 @@ use Twig\TwigFunction;
 require_once('packages/smart-datetime/SmartDatetime.php');
 
 function addCustomExtension(Environment $env) {
+  $package_manifest = json_decode(file_get_contents('packages/patternlab/package.json'), TRUE);
     $env->addExtension(new SmartDatetimeExtension);
-    $env->addFunction(new TwigFunction('get_component_stylesheets', function () {
+    $dependencies = $package_manifest['dependencies'];
+    $env->addFunction(new TwigFunction('get_component_stylesheets', function () use ($dependencies) {
         $styles = '';
-        foreach (glob('packages/*/dist/styles.css') as $component) {
-            $styles .= file_get_contents($component);
+        foreach (array_keys($dependencies) as $dependency) {
+          $component = str_replace('@psu-ooe/', '', $dependency);
+          $css_file = "packages/$component/dist/styles.css";
+          if (file_exists($css_file)) {
+            $styles .= file_get_contents($css_file);
+          }
         }
         return $styles;
     }));
 
-    $env->addFunction(new TwigFunction('get_component_scripts', function () {
+    $env->addFunction(new TwigFunction('get_component_scripts', function () use ($dependencies) {
         $scripts = '';
-        foreach (glob('packages/*/dist/scripts.js') as $component) {
-            $scripts .= file_get_contents($component);
+      foreach (array_keys($dependencies) as $dependency) {
+        $component = str_replace('@psu-ooe/', '', $dependency);
+        $js_file = "packages/$component/dist/scripts.js";
+        if (file_exists($js_file)) {
+          $scripts .= file_get_contents($js_file);
         }
+      }
         return $scripts;
     }));
 
