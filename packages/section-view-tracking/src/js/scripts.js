18e6714d3ddadeeb87da0e004038cb6817b79990
Luke Leber <lal65@psu.edu>
2022-12-01 16:23:42 -0500

    Remove code to fix a bug?!

--- packages/section-view-tracking/src/js/scripts.js
+++ packages/section-view-tracking/src/js/scripts.js
@@ -34,18 +34,7 @@
 
     const components = context.querySelectorAll('[data-interactive-component]');
     components.forEach(component => {
-
-      // Revalidate the section view on activate only on page load.  This works
-      // around a corner-case of a component being activated-by-default on page
-      // load.  Otherwise, the section view would be lost.
       component.addEventListener('component:activate', e => {
-        if (e.detail.activation_type === 'PAGE_LOAD') {
-          revalidate_section_view(component);
-        }
-      });
-
-      // Revalidate the section view upon any successful component activation.
-      component.addEventListener('component:activated', () => {
         revalidate_section_view(component);
       });
     });
