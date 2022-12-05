cc7b1cb470abdff6ea37185023826dc764dfd5f1
Luke Leber <105240977+lal65@users.noreply.github.com>
2022-12-05 10:24:24 -0500

    Section view corner cases (#79)

--- packages/section-view-tracking/src/js/scripts.js
+++ packages/section-view-tracking/src/js/scripts.js
@@ -6,48 +6,45 @@
 
   let current_section = null;
 
-  const revalidate_section_view = component => {
-    const section = component.closest('[data-section]');
-    if (section && section !== current_section) {
-      current_section = section;
-      window.dataLayer = window.dataLayer || [];
-      window.dataLayer.push({
-        event: 'section_view',
-        section_view_title: current_section.getAttribute('data-section'),
-      });
-    }
-  };
-
   cms.attach('sectionViewTracking', context => {
 
-    // On page load, if there's no hash in the URL, or if there is a hash, but
-    // no corresponding element, track a "Header" view.
     if (context === document) {
-      if (!location.hash || !document.querySelector(location.hash)) {
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-          event: 'section_view',
-          section_view_title: 'Header',
-        });
+
+      // @see https://developer.mozilla.org/en-US/docs/Web/API/PerformanceNavigationTiming/type
+      const navigation_type = performance.getEntriesByType('navigation')[0].type;
+
+      // Exclude back/forward, reload, and pre-render navigation types.
+      if (navigation_type === 'navigate') {
+        const hash = location.hash;
+
+        // Only track a "Header" view if there is no fragment, or an invalid fragment.
+        if (!hash || !document.querySelector(hash)) {
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            event: 'section_view',
+            section_view_title: 'Header',
+          });
+        }
       }
     }
 
     const components = context.querySelectorAll('[data-interactive-component]');
-    components.forEach(component => {
 
-      // Revalidate the section view on activate only on page load.  This works
-      // around a corner-case of a component being activated-by-default on page
-      // load.  Otherwise, the section view would be lost.
+    components.forEach(component => {
       component.addEventListener('component:activate', e => {
-        if (e.detail.activation_type === 'PAGE_LOAD') {
-          revalidate_section_view(component);
+        if (e.detail.activation_type !== 'AUTO_EXPAND') {
+          const section = component.closest('[data-section]');
+          if (section && section !== current_section) {
+            current_section = section;
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+              event: 'section_view',
+              section_view_title: current_section.getAttribute('data-section'),
+            });
+          }
         }
       });
-
-      // Revalidate the section view upon any successful component activation.
-      component.addEventListener('component:activated', () => {
-        revalidate_section_view(component);
-      });
     });
+
   });
 })(cms);
