bde610f3be55270fef03ff1d7a2c67d6aed09821
Luke Leber <lal65@psu.edu>
2022-12-09 15:26:44 -0500

    Remove accordion changes

--- packages/accordion/src/js/scripts.js
+++ packages/accordion/src/js/scripts.js
@@ -6,64 +6,25 @@
   cms.attach('accordion', context => {
     const accordions = context.querySelectorAll('.accordion');
     accordions.forEach(accordion => {
-
       const button = accordion.querySelector('.accordion__button');
       const content = accordion.querySelector('.accordion__expandable-content');
+      button.addEventListener('click', () => {
 
-      accordion.addEventListener('component:activate', e => {
-        const state = button.getAttribute('aria-expanded');
-        if (state === 'false') {
+        // Update the transition duration for the open/close animation.
+        const transition = Math.max(content.scrollHeight / 2, 200) + 'ms';
+        button.querySelector('.sprite').style['transition-duration'] = transition;
+        content.style['transition-duration'] = transition;
 
-          accordion.classList.add('accordion--expanded');
-          button.setAttribute('aria-expanded', 'true');
-
-          if (e?.detail?.disable_animation) {
-            content.style.height = null;
-          }
-          else {
-            const transition = Math.max(content.scrollHeight / 2, 200) + 'ms';
-            button.querySelector('.sprite').style['transition-duration'] = transition;
-            content.style['transition-duration'] = transition;
-
-            cms.expand(content);
-          }
-
-
-          accordion.dispatchEvent(new CustomEvent('component:activated'));
-        }
-      });
-
-      accordion.addEventListener('component:deactivate', e => {
         const state = button.getAttribute('aria-expanded');
         if (state === 'true') {
-
           accordion.classList.remove('accordion--expanded');
           button.setAttribute('aria-expanded', 'false');
-
-          if (e?.detail?.disable_animation) {
-            content.style.height = '0px';
-          }
-          else {
-            const transition = Math.max(content.scrollHeight / 2, 200) + 'ms';
-            button.querySelector('.sprite').style['transition-duration'] = transition;
-            content.style['transition-duration'] = transition;
-            cms.collapse(content);
-          }
-
-          accordion.dispatchEvent(new CustomEvent('component:deactivated'));
-        }
-      });
-
-      button.addEventListener('click', () => {
-        if (button.getAttribute('aria-expanded') === 'false') {
-          accordion.dispatchEvent(new CustomEvent('component:activate', {
-            detail: {
-              activation_type: 'USER_ACTIVATE',
-            }
-          }));
+          cms.collapse(content);
         }
         else {
-          accordion.dispatchEvent(new CustomEvent('component:deactivate'));
+          accordion.classList.add('accordion--expanded');
+          button.setAttribute('aria-expanded', 'true');
+          cms.expand(content);
         }
       });
     });
