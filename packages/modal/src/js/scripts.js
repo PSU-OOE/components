e1f868bdfa0fd046b55028cc0be5a15359e10c8d
Brianne Williams <brianne.hennel@gmail.com>
2023-03-21 13:44:58 -0400

    Tweaking twig, CSS, adding JS

--- packages/modal/src/js/scripts.js
+++ packages/modal/src/js/scripts.js
@@ -1,3 +1,16 @@
 const modal_close_sprite = document.querySelector(".modal__close .sprite--fa-times");
+const modal_open_button = document.querySelector(".modal__open");
+const modal_container = document.querySelector(".modal__container");
+const modal_overlay = document.querySelector(".modal__overlay");
 
 modal_close_sprite.setAttribute("alt", "Close");
+
+modal_open_button.addEventListener("click", () => {
+  modal_container.style.display = "flex";
+  modal_overlay.style.display = "flex";
+});
+
+modal_close_sprite.addEventListener("click", () => {
+  modal_container.style.display = "none";
+  modal_overlay.style.display = "none";
+});
