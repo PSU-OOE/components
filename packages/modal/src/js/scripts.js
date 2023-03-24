f498503b5b10a1996a2eff1669349b607ba4956b
Brianne Williams <brianne.hennel@gmail.com>
2023-03-24 11:14:58 -0400

    Refactoring js

--- packages/modal/src/js/scripts.js
+++ packages/modal/src/js/scripts.js
@@ -9,24 +9,34 @@ modal_container.setAttribute("role", "dialog");
 // modal_container.setAttribute("tabindex", "-1");
 modal_svg.setAttribute("tabindex", "0");
 
-// Showing modal
-modal_trigger.addEventListener("click", () => {
+// Functions to show and hide modal
+const showModal = () => {
   modal_container.style.display = "flex";
   modal_overlay.style.display = "flex";
-});
+}
 
-// Hiding modal when clicking on x and moving focus back to triggering element
-modal_close_sprite.addEventListener("click", () => {
+const hideModal = () => {
   modal_container.style.display = "none";
   modal_overlay.style.display = "none";
   modal_trigger.focus();
+}
+
+// Showing modal
+modal_trigger.addEventListener("click", showModal)
+
+// Hiding modal when clicking on x and moving focus back to triggering element
+modal_close_sprite.addEventListener("click", hideModal);
+
+// Hiding modal when hitting the enter key on x and moving focus back to triggering element
+modal_svg.addEventListener("keyup", (e) => {
+  if (e.key === 'Enter' || e.keyCode === 13) {
+    hideModal();
+  }
 });
 
 // Hiding modal when clicking outside of it and moving focus back to triggering element
 modal_overlay.addEventListener("click", (e) => {
   if ((e.target == modal_overlay) && (modal_container.style.display == "flex") && (modal_overlay.style.display == "flex")) {
-    modal_container.style.display = "none";
-    modal_overlay.style.display = "none";
-    modal_trigger.focus();
+    hideModal();
   }
 });
