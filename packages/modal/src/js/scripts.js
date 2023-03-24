6f491379d2c394269c5b77a1e5b40d11b89560bd
Brianne Williams <brianne.hennel@gmail.com>
2023-03-24 10:56:32 -0400

    Moving focus back to trigger on modal close, adding comments to js code

--- packages/modal/src/js/scripts.js
+++ packages/modal/src/js/scripts.js
@@ -9,19 +9,24 @@ modal_container.setAttribute("role", "dialog");
 // modal_container.setAttribute("tabindex", "-1");
 modal_svg.setAttribute("tabindex", "0");
 
+// Showing modal
 modal_trigger.addEventListener("click", () => {
   modal_container.style.display = "flex";
   modal_overlay.style.display = "flex";
 });
 
+// Hiding modal when clicking on x and moving focus back to triggering element
 modal_close_sprite.addEventListener("click", () => {
   modal_container.style.display = "none";
   modal_overlay.style.display = "none";
+  modal_trigger.focus();
 });
 
+// Hiding modal when clicking outside of it and moving focus back to triggering element
 modal_overlay.addEventListener("click", (e) => {
   if ((e.target == modal_overlay) && (modal_container.style.display == "flex") && (modal_overlay.style.display == "flex")) {
     modal_container.style.display = "none";
     modal_overlay.style.display = "none";
+    modal_trigger.focus();
   }
 });
