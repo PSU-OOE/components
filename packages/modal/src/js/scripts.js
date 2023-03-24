a077345d8c9fdf08f65c865346fcf80a46f52849
Brianne Williams <brianne.hennel@gmail.com>
2023-03-24 10:40:40 -0400

    Adding a link to twig, moving around some css styles, making svg focusable

--- packages/modal/src/js/scripts.js
+++ packages/modal/src/js/scripts.js
@@ -2,10 +2,12 @@ const modal_close_sprite = document.querySelector(".modal__close .sprite--fa-tim
 const modal_open_button = document.querySelector(".modal__open");
 const modal_container = document.querySelector(".modal__container");
 const modal_overlay = document.querySelector(".modal__overlay");
+const modal_svg = document.querySelector(".modal__close .sprite--fa-times")
 
 modal_close_sprite.setAttribute("alt", "Close");
 modal_container.setAttribute("role", "dialog");
 modal_container.setAttribute("tabindex", "-1");
+modal_svg.setAttribute("tabindex", "0");
 
 modal_open_button.addEventListener("click", () => {
   modal_container.style.display = "flex";
