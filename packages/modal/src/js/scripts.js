64c6099e9c253aa981a86fb221b72025be10ff5c
Brianne Williams <brianne.hennel@gmail.com>
2023-04-06 10:04:07 -0400

    Luke's JS and twig updates, a couple of style changes.

--- packages/modal/src/js/scripts.js
+++ packages/modal/src/js/scripts.js
@@ -1,60 +1,66 @@
-// Variables
-const modals = document.querySelectorAll(".modal");
+(cms => {
+  cms.attach('modal', context => {
+    // Variables
+    const modals = document.querySelectorAll(".modal");
 
-// Functions
-const showModal = (element) => {
-  element.classList.add("show-modal");
-  element.classList.remove("hide-modal");
-};
+    // Functions
+    const showModal = (element) => {
+      element.classList.add("show-modal");
+      element.classList.remove("hide-modal");
+    };
 
-const hideModal = (element) => {
-  element.classList.add("hide-modal");
-  element.classList.remove("show-modal");
-};
+    const hideModal = (element) => {
+      element.classList.add("hide-modal");
+      element.classList.remove("show-modal");
+    };
 
-const moveFocus = (element) => {
-  element.focus();
-};
+    const moveFocus = (element) => {
+      element.focus();
+    };
 
-// TODO: All functionality to return focus to the triggering element will likely need to be refactored
-// based on final implementation/HTML/DOM structure. For now, it's a button that is the previous direct sibling
-// to the outer .modal div
+    // TODO: All functionality to return focus to the triggering element will likely need to be refactored
+    // based on final implementation/HTML/DOM structure. For now, it's a button that is the previous direct sibling
+    // to the outer .modal div
 
-modals.forEach((modal) => {
-  const modalTrigger = modal.previousElementSibling;
-  const modalOverlay = modal.firstElementChild;
-  const modalContainer = modalOverlay.firstElementChild;
-  const modalCloseBtn = modalContainer.firstElementChild;
-  const modalCloseBtnSvg = modalCloseBtn.firstElementChild;
+    modals.forEach((modal) => {
+      const modalOverlay = modal.firstElementChild;
+      const modalContainer = modalOverlay.firstElementChild;
+      const modalCloseBtn = modalContainer.firstElementChild;
 
-  // Attributes
-  modalCloseBtnSvg.setAttribute("aria-hidden", "true");
+      modal.addEventListener('component:activate', e => {
+        if (!modal.classList.contains('show-modal')) {
+          modal.modalTrigger = e.detail.modal_trigger;
+          showModal(modal);
+          moveFocus(modalContainer);
+        }
+      });
 
-  // Showing modal when clicking triggering element and moving focus to modal container
-  modalTrigger.addEventListener("click", (e) => {
-    showModal(modal);
-    moveFocus(modalContainer);
+      modal.addEventListener('component:deactivate', e => {
+        if (!modal.classList.contains('hide-modal')) {
+          hideModal(modal);
+          moveFocus(modal.modalTrigger);
+          modal.modalTrigger = null;
+        }
+      });
+
+      // Hiding modal and returning focus to triggering element after clicking/hitting enter key on close button
+      modalCloseBtn.addEventListener("click", (e) => {
+        modal.dispatchEvent(new CustomEvent('component:deactivate'));
+      });
+
+      // Hiding modal when clicking outside of it and moving focus back to triggering element
+      modalOverlay.addEventListener("click", (e) => {
+        if ((e.target == modalOverlay) && (modal.classList.contains("show-modal"))) {
+          modal.dispatchEvent(new CustomEvent('component:deactivate'));
+        }
+      });
+
+      // Hiding modal when hitting the escape key and moving focus back to triggering element
+      modal.addEventListener("keyup", (e) => {
+        if ((e.key == "Escape") || (e.keycode === 27) && (modal.classList.contains("show-modal"))) {
+          modal.dispatchEvent(new CustomEvent('component:deactivate'));
+        }
+      })
+    });
   });
-
-  // Hiding modal and returning focus to triggering element after clicking/hitting enter key on close button
-  modalCloseBtn.addEventListener("click", (e) => {
-    hideModal(modal);
-    moveFocus(modalTrigger);
-  });
-
-  // Hiding modal when clicking outside of it and moving focus back to triggering element
-  modalOverlay.addEventListener("click", (e) => {
-    if ((e.target == modalOverlay) && (modal.classList.contains("show-modal"))) {
-      hideModal(modal);
-      moveFocus(modalTrigger);
-    }
-  });
-
-  // Hiding modal when hitting the escape key and moving focus back to triggering element
-  modal.addEventListener("keyup", (e) => {
-    if ((e.key == "Escape") || (e.keycode === 27) && (modal.classList.contains("show-modal"))) {
-      hideModal(modal);
-      moveFocus(modalTrigger);
-    }
-  })
-});
+})(cms);
