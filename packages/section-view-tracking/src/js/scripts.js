932aaff2a75677055152061e406dcd110a0f81aa
Luke Leber <105240977+lal65@users.noreply.github.com>
2023-07-13 10:02:15 -0400

    Section view tags (#179)

--- packages/section-view-tracking/src/js/scripts.js
+++ packages/section-view-tracking/src/js/scripts.js
@@ -37,10 +37,20 @@
           if (section && section !== current_section) {
             current_section = section;
             window.dataLayer = window.dataLayer || [];
-            window.dataLayer.push({
-              event: 'section_view',
-              section_view_title: current_section.getAttribute('data-section'),
-            });
+
+            try {
+              let tags = JSON.parse(current_section.getAttribute('data-section-tags') ?? '{}');
+              tags.activation_type = e.detail.activation_type;
+              window.dataLayer.push({
+                event: 'section_view',
+                section_view_title: current_section.getAttribute('data-section'),
+                tags: tags,
+              });
+            }
+            catch(error) {
+              // Prevent unwinding the stack; log the error and continue on.
+              console.error(error);
+            }
           }
         }
       });
